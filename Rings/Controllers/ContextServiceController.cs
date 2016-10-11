602ad6ba84e1c44cc1c714dd81ad412b9db6ba0c
1194420729 <1194420729@qq.com>
2016-10-11 11:33:59 +0800

    set contextservice visit just only for localhost

--- Rings/Controllers/ContextServiceController.cs
+++ Rings/Controllers/ContextServiceController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rings.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,7 +7,7 @@ using System.Web.Mvc;
 
 namespace Rings.Controllers
 {
-    [Authorize]
+    [LocalhostOnlyAttribute]
     public class ContextServiceController : Controller
     { 
         public ActionResult MapPath(string path)

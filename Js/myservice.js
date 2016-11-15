56264962fb9a64255f1fda0e14bc41305b3a2028
1194420729 <1194420729@qq.com>
2016-11-15 15:17:38 +0800

    set menu selected

---
+++ Js/myservice.js
@@ -0,0 +1,68 @@
+﻿angular.module('myservice', []);
+
+angular.module('myservice').service('mytools', function () {
+    this.toFixedNumber = function (n, d) {
+        return parseFloat(n.toFixed(d));
+    };
+
+    this.qtyChange = function (obj, digit) {
+        if (obj.qty && obj.price !== undefined) {
+            obj.total = this.toFixedNumber(obj.qty * obj.price, digit);
+            obj.taxtotal = this.toFixedNumber(obj.total * (100 + obj.taxrate) / 100, digit);
+            obj.taxprice = this.toFixedNumber(obj.price * (100 + obj.taxrate) / 100, digit);
+            obj.discounttotal = this.toFixedNumber(obj.taxtotal * obj.discountrate / 100, digit);
+            obj.discountprice = this.toFixedNumber(obj.taxprice * obj.discountrate / 100, digit);
+        }
+    };
+     
+    this.totalChange = function () {
+        if (obj.qty && obj.total !== undefined) {
+            obj.price = this.toFixedNumber(obj.total / obj.qty, digit);
+            obj.taxtotal = this.toFixedNumber(obj.total * (100 + obj.taxrate) / 100, digit);
+            obj.taxprice = this.toFixedNumber(obj.price * (100 + obj.taxrate) / 100, digit);
+            obj.discounttotal = this.toFixedNumber(obj.taxtotal * obj.discountrate / 100, digit);
+            obj.discountprice = this.toFixedNumber(obj.taxprice * obj.discountrate / 100, digit);
+        }
+    };
+
+    this.taxpriceChange = function () {
+        if (obj.qty && obj.taxprice !== undefined) {
+            obj.price = this.toFixedNumber(obj.taxprice / (1 + obj.taxrate / 100), digit);
+            obj.total = this.toFixedNumber(obj.qty * obj.price, digit);
+            obj.taxtotal = this.toFixedNumber(obj.qty * obj.taxprice, digit);
+            obj.discounttotal = this.toFixedNumber(obj.taxtotal * obj.discountrate / 100, digit);
+            obj.discountprice = this.toFixedNumber(obj.taxprice * obj.discountrate / 100, digit);
+        }
+    };
+
+    this.taxtotalChange = function () {
+        if (obj.qty && obj.taxtotal !== undefined) {
+            obj.taxprice = this.toFixedNumber(obj.taxtotal / obj.qty, digit);
+            obj.price = this.toFixedNumber(obj.taxprice / (1 + obj.taxrate / 100), digit);
+            obj.total = this.toFixedNumber(obj.qty * obj.price, digit);
+            obj.discounttotal = this.toFixedNumber(obj.taxtotal * obj.discountrate / 100, digit);
+            obj.discountprice = this.toFixedNumber(obj.taxprice * obj.discountrate / 100, digit);
+        }
+    };
+
+    this.discountpriceChange = function () {
+        if (obj.qty && obj.discountprice !== undefined) {
+            obj.discounttotal = this.toFixedNumber(obj.discountprice * obj.qty, digit);
+            obj.taxprice = this.toFixedNumber(obj.discountprice / obj.discountrate * 100, digit);
+            obj.taxtotal = this.toFixedNumber(obj.taxprice * obj.qty, digit);
+            obj.price = this.toFixedNumber(obj.taxprice / (1 + obj.taxrate / 100), digit);
+            obj.total = this.toFixedNumber(obj.qty * obj.price, digit);
+        }
+    };
+
+    this.discounttotalChange = function () {
+        if (obj.qty && obj.discounttotal !== undefined) {
+            obj.discountprice = this.toFixedNumber(obj.discounttotal / obj.qty, digit);
+            obj.taxprice = this.toFixedNumber(obj.discountprice / obj.discountrate * 100, digit);
+            obj.taxtotal = this.toFixedNumber(obj.taxprice * obj.qty, digit);
+            obj.price = this.toFixedNumber(obj.taxprice / (1 + obj.taxrate / 100), digit);
+            obj.total = this.toFixedNumber(obj.qty * obj.price, digit);
+        }
+    };
+
+});
---
+++ Js/ngnext.js
@@ -0,0 +1,35 @@
+﻿(function (angular) {
+    'use strict';
+
+    if (!angular) {
+        throw 'Missing something? Please add angular.js to your project or move this script below the angular.js reference';
+    }
+
+    function ngNextDirective() {
+        var obj = {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                if (attr.ngNext==='true') {
+                    var focusables = angular.element('input');
+                    element.bind('keydown', function (e) {
+                        var code = e.keyCode || e.which; 
+                        if (code === 13) { 
+                            e.preventDefault();
+                            var current = focusables.index(this);
+                            var nextinput = focusables.eq(current + 1).length ? focusables.eq(current + 1) : focusables.eq(0);
+                            //console.log(angular.toJson(nextinput));
+                            nextinput.focus();
+                        }
+                    });
+                }
+            }
+        };
+
+        return obj;
+    }
+
+    //// Angular Code ////
+    var mi = angular.module('ngNext', []);
+    mi.directive('ngNext', ngNextDirective);
+
+})(angular);

911b7744ecf54a15ac675f79c1df53f59ff5fcfb
1194420729 <1194420729@qq.com>
2017-05-03 16:12:24 +0800

    more bill names and grid calc tools update

--- Js/myservice.js
+++ Js/myservice.js
@@ -7,22 +7,22 @@ angular.module('myservice').service('mytools', function () {
 
     this.qtyChange = function (obj, digit) {
         if (obj.qty && obj.price !== undefined && obj.price !== null) {
-            obj.total = this.toFixedNumber(obj.qty * obj.price, digit);            
+            obj.total = this.toFixedNumber(obj.qty * obj.price, digit);
             obj.taxprice = this.toFixedNumber(obj.price * (100 + obj.taxrate) / 100, digit);
-			obj.taxtotal = this.toFixedNumber(obj.taxprice * obj.qty, digit);
+            obj.taxtotal = this.toFixedNumber(obj.taxprice * obj.qty, digit);
             obj.discounttotal = this.toFixedNumber(obj.taxtotal * obj.discountrate / 100, digit);
             obj.discountprice = this.toFixedNumber(obj.taxprice * obj.discountrate / 100, digit);
         }
     };
-     
+
     this.totalChange = function (obj, digit) {
         if (obj.total === undefined || obj.total === null) return;
 
         obj.taxtotal = this.toFixedNumber(obj.total * (100 + obj.taxrate) / 100, digit);
         obj.discounttotal = this.toFixedNumber(obj.taxtotal * obj.discountrate / 100, digit);
         if (obj.qty) {
-            obj.price = this.toFixedNumber(obj.total / obj.qty, digit);            
-            obj.taxprice = this.toFixedNumber(obj.price * (100 + obj.taxrate) / 100, digit);           
+            obj.price = this.toFixedNumber(obj.total / obj.qty, digit);
+            obj.taxprice = this.toFixedNumber(obj.price * (100 + obj.taxrate) / 100, digit);
             obj.discountprice = this.toFixedNumber(obj.taxprice * obj.discountrate / 100, digit);
         }
     };
@@ -32,10 +32,10 @@ angular.module('myservice').service('mytools', function () {
 
         obj.price = this.toFixedNumber(obj.taxprice / (1 + obj.taxrate / 100), digit);
         obj.discountprice = this.toFixedNumber(obj.taxprice * obj.discountrate / 100, digit);
-        if (obj.qty) {            
+        if (obj.qty) {
             obj.total = this.toFixedNumber(obj.qty * obj.price, digit);
             obj.taxtotal = this.toFixedNumber(obj.qty * obj.taxprice, digit);
-            obj.discounttotal = this.toFixedNumber(obj.taxtotal * obj.discountrate / 100, digit);            
+            obj.discounttotal = this.toFixedNumber(obj.taxtotal * obj.discountrate / 100, digit);
         }
     };
 
@@ -46,7 +46,7 @@ angular.module('myservice').service('mytools', function () {
         obj.discounttotal = this.toFixedNumber(obj.taxtotal * obj.discountrate / 100, digit);
         if (obj.qty) {
             obj.taxprice = this.toFixedNumber(obj.taxtotal / obj.qty, digit);
-            obj.price = this.toFixedNumber(obj.taxprice / (1 + obj.taxrate / 100), digit);            
+            obj.price = this.toFixedNumber(obj.taxprice / (1 + obj.taxrate / 100), digit);
             obj.discountprice = this.toFixedNumber(obj.taxprice * obj.discountrate / 100, digit);
         }
     };
@@ -57,8 +57,8 @@ angular.module('myservice').service('mytools', function () {
         obj.taxprice = this.toFixedNumber(obj.discountprice / obj.discountrate * 100, digit);
         obj.price = this.toFixedNumber(obj.taxprice / (1 + obj.taxrate / 100), digit);
         if (obj.qty) {
-            obj.discounttotal = this.toFixedNumber(obj.discountprice * obj.qty, digit);            
-            obj.taxtotal = this.toFixedNumber(obj.taxprice * obj.qty, digit);            
+            obj.discounttotal = this.toFixedNumber(obj.discountprice * obj.qty, digit);
+            obj.taxtotal = this.toFixedNumber(obj.taxprice * obj.qty, digit);
             obj.total = this.toFixedNumber(obj.qty * obj.price, digit);
         }
     };
@@ -70,9 +70,17 @@ angular.module('myservice').service('mytools', function () {
         obj.total = this.toFixedNumber(obj.taxtotal / (1 + obj.taxrate / 100), digit);
         if (obj.qty) {
             obj.discountprice = this.toFixedNumber(obj.discounttotal / obj.qty, digit);
-            obj.taxprice = this.toFixedNumber(obj.discountprice / obj.discountrate * 100, digit);            
-            obj.price = this.toFixedNumber(obj.taxprice / (1 + obj.taxrate / 100), digit);            
+            obj.taxprice = this.toFixedNumber(obj.discountprice / obj.discountrate * 100, digit);
+            obj.price = this.toFixedNumber(obj.taxprice / (1 + obj.taxrate / 100), digit);
         }
+    };
+
+    this.checkpositivenumeric = function (num) {
+        if (num === undefined || num === null || num < 0) {
+            return false;
+        }
+
+        return true;
     };
 
 });
